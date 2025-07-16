74e5f6762ea2aa7d307371977d21e270853fec52
KrithikPatil <patilkrithik@gmail.com>
2025-07-16 18:57:47 +0530

    Working backend for windows

--- services/pinecone_service.py
+++ services/pinecone_service.py
@@ -1,9 +1,11 @@
 # service/pinecone_service.py
 
 import os
+from dotenv import load_dotenv
 from pinecone import Pinecone
 from typing import List, Dict, Any
 
+load_dotenv()
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 pc = Pinecone(api_key=PINECONE_API_KEY)
 
